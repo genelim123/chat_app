8089b0ca2060936e70b7d35c58a6f225e321d6ae
genelim123 <wjelim001@mymail.sim.edu.sg>
2024-04-14 01:04:50 +0800

    Improved Sign Up Endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,16 +3,16 @@ import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
     try {
-        const {fullName, username, password, confirmPassword,gender} = req.body;
+        const { fullName, username, password, confirmPassword, gender } = req.body;
 
         if (password !== confirmPassword) {
-            return res.status(400).json({message: "Passwords do not match"});
+            return res.status(400).json({ message: "Passwords do not match" });
         }
 
-        const user = await User.findOne({username});
+        const user = await User.findOne({ username });
 
-        if(user) {
-            return res.status(400).json({message: "User already exists"});
+        if (user) {
+            return res.status(400).json({ message: "User already exists" });
         }
 
         // HASH PASSWORD HERE
@@ -23,26 +23,30 @@ export const signup = async (req, res) => {
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
 
-        const newUser = new User ({
+        const newUser = new User({
             fullName,
             username,
             password: hashedPassword,
             gender,
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
-        })
+        });
 
-        await newUser.save();
+        if (newUser) {
 
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        })
+            await newUser.save();
 
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            });
+        } else {
+            res.status(400).json({ error: "Invalid User Data" });
+        }
     } catch (error) {
         console.log("Error in signup controller", error.message)
-        res.status(500).json({error:"Internal Server Error"})
+        res.status(500).json({ error: "Internal Server Error" })
     }
 };
 
