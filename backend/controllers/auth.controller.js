886cb96f7bb94ca012b250ef0d4721bfc014ca48
genelim123 <wjelim001@mymail.sim.edu.sg>
2024-04-14 01:01:32 +0800

    Implemented Sign Up Endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,48 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
     try {
-        const {fullName, userName, password, confirmedPassword,gender} = req.body;
+        const {fullName, username, password, confirmPassword,gender} = req.body;
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({message: "Passwords do not match"});
+        }
+
+        const user = await User.findOne({username});
+
+        if(user) {
+            return res.status(400).json({message: "User already exists"});
+        }
+
+        // HASH PASSWORD HERE
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        // Profile Picture API: https://avatar-placeholder.iran.liara.run/
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User ({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+        })
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic
+        })
+
     } catch (error) {
-        console.log(error);
+        console.log("Error in signup controller", error.message)
+        res.status(500).json({error:"Internal Server Error"})
     }
 };
 
