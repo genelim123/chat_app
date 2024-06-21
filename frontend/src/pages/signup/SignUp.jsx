0f815d0074161214ccfdc2b24df9b1fd30ad893f
genelim123 <wjelim001@mymail.sim.edu.sg>
2024-06-22 03:10:14 +0800

    Implemented Frontend designs for Sidebar, Message

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import GenderCheckbox from './GenderCheckbox'
 
 const SignUp = () => {
     return (
@@ -54,8 +55,9 @@ const SignUp = () => {
                     </div>
 
                     {/* GENDER CHECKBOX GOES HERE */}
+                    <GenderCheckbox/>
 
-                    <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-4 inline-block'>
+                    <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
                         Already have an account?
                     </a>
 
@@ -71,3 +73,82 @@ const SignUp = () => {
 }
 
 export default SignUp
+
+
+// STARTER CODE FOR THE SIGNUP COMPONENT
+// import React from 'react'
+// import GenderCheckbox from './GenderCheckbox'
+
+// const SignUp = () => {
+//     return (
+//         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+//             <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+//                 <h1 className='text-3xl font-semibold text-center text-gray-300'>
+//                     Sign up <span className='text-blue-500'>Chat App</span>
+//                 </h1>
+
+//                 <form>
+//                     <div>
+//                         <label className='label p-2'>
+//                             <span className='text-base label-text'>Full Name</span>
+//                         </label>
+//                         <input
+//                             type='text'
+//                             placeholder='Enter full name'
+//                             className='w-full input input-bordered h-10'
+//                         />
+//                     </div>
+
+//                     <div>
+//                         <label className='label p-2'>
+//                             <span className='text-base label-text'>Username</span>
+//                         </label>
+//                         <input
+//                             type='text'
+//                             placeholder='Enter username'
+//                             className='w-full input input-bordered h-10'
+//                         />
+//                     </div>
+
+//                     <div>
+//                         <label className='label p-2'>
+//                             <span className='text-base label-text'>Password</span>
+//                         </label>
+//                         <input
+//                             type='text'
+//                             placeholder='Enter password'
+//                             className='w-full input input-bordered h-10'
+//                         />
+//                     </div>
+
+//                     <div>
+//                         <label className='label p-2'>
+//                             <span className='text-base label-text'>Confirm Password</span>
+//                         </label>
+//                         <input
+//                             type='text'
+//                             placeholder='Enter password again'
+//                             className='w-full input input-bordered h-10'
+//                         />
+//                     </div>
+
+//                     {/* GENDER CHECKBOX GOES HERE */}
+//                     <GenderCheckbox/>
+
+//                     <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+//                         Already have an account?
+//                     </a>
+
+//                     <div>
+//                         <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign up</button>
+//                     </div>
+
+//                 </form>
+
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default SignUp
+
